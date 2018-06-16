1aafd1e59f82edf3858da6bfad50ca715492afda
BuildTools <unconfigured@null.spigotmc.org>
2018-06-16 22:21:10 +0200

    check if the core is installed or not

--- src/main/java/com/twanl/minigame/MiniGame.java
+++ src/main/java/com/twanl/minigame/MiniGame.java
@@ -1,10 +1,25 @@
 package com.twanl.minigame;
 
+import com.twanl.minigame.util.Strings;
+import nl.RamonPeek.API;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MiniGame extends JavaPlugin {
 
+
+    private API api = (API) Bukkit.getServer().getPluginManager().getPlugin("Core");
+
+    @EventHandler
     public void onEnable() {
+        if (getServer().getPluginManager().getPlugin("Core") != null) {
+            getServer().getConsoleSender().sendMessage(Strings.green + Strings.logName + "Core HOOKED.");
+        } else {
+            getServer().getConsoleSender().sendMessage(Strings.green + Strings.logName + Strings.red + "Core not found, plugin will be disabled");
+            Bukkit.getServer().getPluginManager().disablePlugins();
+        }
+
 
     }
 
