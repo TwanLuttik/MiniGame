32fca21b984fca0a8818fb1fc51a0099ee0a1e55
BuildTools <unconfigured@null.spigotmc.org>
2018-06-16 21:55:18 +0200

    Initial commit

---
+++ src/main/java/com/twanl/minigame/MiniGame.java
@@ -0,0 +1,14 @@
+package com.twanl.minigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MiniGame extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
